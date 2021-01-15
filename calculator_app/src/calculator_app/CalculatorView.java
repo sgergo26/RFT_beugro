a89c04d9b44d6d737c3897fc07789a2437db01da
bnoemi9808 <bnoemi9830@gmail.com>
2021-01-15 12:37:50 +0100

    Noémi projekt 3. commit: A kijelzőt szimbolizáló szövegmező deklarálása

--- calculator_app/src/calculator_app/CalculatorView.java
+++ calculator_app/src/calculator_app/CalculatorView.java
@@ -5,5 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class CalculatorView {
+	// A szövegmező deklarálása
+    private JTextField display;
     
 }
