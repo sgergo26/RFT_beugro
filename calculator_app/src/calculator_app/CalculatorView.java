ad7484edbfc8cd932c17ca4f7a21f2a45fe00d43
bnoemi9808 <bnoemi9830@gmail.com>
2021-01-15 12:41:59 +0100

    Noémi projekt 4. commit: A szövegmezű betűtípusának megjelenítési beállításai ( típus, méret, vastagság)

--- calculator_app/src/calculator_app/CalculatorView.java
+++ calculator_app/src/calculator_app/CalculatorView.java
@@ -7,5 +7,7 @@ import java.awt.event.ActionListener;
 public class CalculatorView {
 	// A szövegmező deklarálása
     private JTextField display;
+	// A szövegmező betűtípus, betűméret és betűvastagságának beállítása
+    private static final Font BOLD_FONT = new Font(Font.MONOSPACED, Font.BOLD, 20);
     
 }
