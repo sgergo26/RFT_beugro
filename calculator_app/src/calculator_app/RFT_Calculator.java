9d12f4e5e78e033eae5223a47e99b1b388b7a423
sgergo26 <77020820+sgergo26@users.noreply.github.com>
2021-01-15 18:36:17 +0100

    Állományok összehangolása III.
    Az összehangolt állományokat, egybe foglaló main osztály

---
+++ calculator_app/src/calculator_app/RFT_Calculator.java
@@ -0,0 +1,14 @@
+
+package rft_calculator;
+import javax.swing.*;
+
+
+public class RFT_Calculator {
+
+    public static void main(String[] args)throws Exception{
+        CalculatorView calc = new CalculatorView(){};
+        calc.setTitle("Szamologep");
+        calc.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    
+}
