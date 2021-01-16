b2f316ae494b39acb3871b459786f5ed3114775d
sgergo26 <77020820+sgergo26@users.noreply.github.com>
2021-01-16 10:56:27 +0100

    Üdvözlő üzenet és aktuális verziószám hozzáadása

--- calculator_app/src/calculator_app/WelcomeWindow.java
+++ calculator_app/src/calculator_app/WelcomeWindow.java
@@ -6,14 +6,29 @@ import javax.swing.*;
 
 public class WelcomeWindow extends JFrame{
     public WelcomeWindow() {
+        // az ablak konfigurálása (méret, átméretezhetőség, stb)
         Dimension size = new Dimension(320,300);
         setPreferredSize(size);
         setResizable(false);
         
-        JPanel wp = new JPanel();
+        //A cím kialakítása
+        JLabel title = new JLabel("Üdvözli a Java Számológép");
+        title.setFont(new Font("Dialog",Font.BOLD,18));
         
+        //A verizószám megjelenítése
+        JLabel version = new JLabel("Ön jelenleg a v1.2 verziót használja");
+        version.setFont(new Font("Dialog",Font.PLAIN,12));
+        
+        //az ablak vázát adó főpanel összeállítása.
+        JPanel wp = new JPanel();
+        wp.add(title);
+        wp.add(version);
+        
+        
+        // az ablak megjelenítése
         setContentPane(wp);
         pack();
         setVisible(true);
+
     }
 }
