24ad6e173cb21e0502c66df76e4c64d92ac1dc03
sgergo26 <77020820+sgergo26@users.noreply.github.com>
2021-01-16 16:14:13 +0100

    Rövid súgó hozzáadása
    Az üdvözlő képernyőre egy rövid súgószöveg került a program használatáról

--- calculator_app/src/calculator_app/WelcomeWindow.java
+++ calculator_app/src/calculator_app/WelcomeWindow.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class WelcomeWindow extends JFrame{
     public WelcomeWindow() {
         // az ablak konfigurálása (méret, átméretezhetőség, stb)
-        Dimension size = new Dimension(320,300);
+        Dimension size = new Dimension(450,500);
         setPreferredSize(size);
         setResizable(false);
         
@@ -16,13 +16,32 @@ public class WelcomeWindow extends JFrame{
         title.setFont(new Font("Dialog",Font.BOLD,18));
         
         //A verizószám megjelenítése
-        JLabel version = new JLabel("Ön jelenleg a v1.2 verziót használja");
+        JLabel version = new JLabel("Ön jelenleg a v1.3 verziót használja");
         version.setFont(new Font("Dialog",Font.PLAIN,12));
+        
+        //Rövid súgó
+        JPanel helppanel = new JPanel();
+        helppanel.setLayout(new GridLayout(6,0));
+        JLabel helplabel = new JLabel("Súgó:");
+        JLabel help1 = new JLabel("1. Adja meg az első számot!");
+        JLabel help2 = new JLabel("2. Kattintson az elvégezni kívánt műveletre!");
+        JLabel help3 = new JLabel("3. Adja meg a második számot!");
+        JLabel help4 = new JLabel("4. Ha további műveleteket szeretne megadni térjen vissza a 2-es pontra!");
+        JLabel help5 = new JLabel("5. Írassa ki az eredményt az (=) gommbal!");
+        
+        helppanel.add(helplabel);
+        helppanel.add(help1);
+        helppanel.add(help2);
+        helppanel.add(help3);
+        helppanel.add(help4);
+        helppanel.add(help5);
+        
         
         //az ablak vázát adó főpanel összeállítása.
         JPanel wp = new JPanel();
         wp.add(title);
         wp.add(version);
+        wp.add(helppanel);
         
         
         // az ablak megjelenítése
