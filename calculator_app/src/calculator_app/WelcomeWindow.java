b085fee7847804186158c1b1ac4addc23db29d62
sgergo26 <77020820+sgergo26@users.noreply.github.com>
2021-01-16 17:02:34 +0100

    Szerzők neveinek hozzáadása
    Az üdvözlő képernyőre felkerültek a szerzők nevei, a súgó formai átalakításon esett át a jobb tagoltság érdekében.

--- calculator_app/src/calculator_app/WelcomeWindow.java
+++ calculator_app/src/calculator_app/WelcomeWindow.java
@@ -16,18 +16,24 @@ public class WelcomeWindow extends JFrame{
         title.setFont(new Font("Dialog",Font.BOLD,18));
         
         //A verizószám megjelenítése
-        JLabel version = new JLabel("Ön jelenleg a v1.3 verziót használja");
+        JLabel version = new JLabel("Ön jelenleg a v1.4 verziót használja");
         version.setFont(new Font("Dialog",Font.PLAIN,12));
         
         //Rövid súgó
         JPanel helppanel = new JPanel();
         helppanel.setLayout(new GridLayout(6,0));
         JLabel helplabel = new JLabel("Súgó:");
+        helplabel.setFont(new Font("Dialog",Font.BOLD,14));
         JLabel help1 = new JLabel("1. Adja meg az első számot!");
+        help1.setFont(new Font("Dialog",Font.PLAIN,12));
         JLabel help2 = new JLabel("2. Kattintson az elvégezni kívánt műveletre!");
+        help2.setFont(new Font("Dialog",Font.PLAIN,12));
         JLabel help3 = new JLabel("3. Adja meg a második számot!");
+        help3.setFont(new Font("Dialog",Font.PLAIN,12));
         JLabel help4 = new JLabel("4. Ha további műveleteket szeretne megadni térjen vissza a 2-es pontra!");
+        help4.setFont(new Font("Dialog",Font.PLAIN,12));
         JLabel help5 = new JLabel("5. Írassa ki az eredményt az (=) gommbal!");
+        help5.setFont(new Font("Dialog",Font.PLAIN,12));
         
         helppanel.add(helplabel);
         helppanel.add(help1);
@@ -36,13 +42,46 @@ public class WelcomeWindow extends JFrame{
         helppanel.add(help4);
         helppanel.add(help5);
         
+        //Szerzők cím
+        JPanel authorLabel = new JPanel();
+        JLabel authorTitle = new JLabel("A Program szerzői:");
+        authorTitle.setFont(new Font("Dialog",Font.BOLD,16));
+        authorLabel.add(authorTitle);
+        
+        //Szerzők listája
+        JPanel authors = new JPanel();
+        GridLayout agl = new GridLayout();
+        agl.setColumns(2);
+        agl.setRows(4);
+        agl.setHgap(20);
+        authors.setLayout(agl);
+        
+        JLabel av = new JLabel ("Ardó Virgínia Katalin");
+        JLabel avn = new JLabel ("FJ7N0W");
+        JLabel bn = new JLabel ("Banos Noémi");
+        JLabel bnn = new JLabel ("U75RBH");
+        JLabel hg = new JLabel ("Horváth Gabriella");
+        JLabel hgn = new JLabel ("R8X8R6");
+        JLabel sg = new JLabel ("Seres Gergő");
+        JLabel sgn = new JLabel ("HMX501");
+        
+        authors.add(av);
+        authors.add(avn);
+        authors.add(bn);
+        authors.add(bnn);
+        authors.add(hg);
+        authors.add(hgn);
+        authors.add(sg);
+        authors.add(sgn);
+        
         
         //az ablak vázát adó főpanel összeállítása.
         JPanel wp = new JPanel();
         wp.add(title);
         wp.add(version);
         wp.add(helppanel);
-        
+        wp.add(authorLabel);
+        wp.add(authors);
         
         // az ablak megjelenítése
         setContentPane(wp);
