1391e9b822cc31f6536d8ee4a3ecb1a02ce0d111
sgergo26 <77020820+sgergo26@users.noreply.github.com>
2021-01-16 20:55:04 +0100

    Kommentek beszúrása
    Az érthetőbb kód érdekében

--- calculator_app/src/calculator_app/RFT_Calculator.java
+++ calculator_app/src/calculator_app/RFT_Calculator.java
@@ -6,13 +6,15 @@ import javax.swing.*;
 public class RFT_Calculator {
 
     public static void main(String[] args)throws Exception{
+        
+        //Az üdvözlőképernyő példányosítása
         WelcomeWindow ww = new WelcomeWindow(){};
         ww.setTitle("Üdvözlet");
         ww.setAlwaysOnTop(true);
         
+        //A főképernyő példányosítása
         CalculatorView calc = new CalculatorView(){};
         calc.setTitle("Szamologep");
         calc.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    }
-    
+    }  
 }
