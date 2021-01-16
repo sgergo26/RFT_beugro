e1665855bfa1f5b50a84ca8fbf2542ade5af9db2
sgergo26 <77020820+sgergo26@users.noreply.github.com>
2021-01-16 10:06:24 +0100

    Állományok összehangolása IV.
    Az üdvözlőképernyő példányosítása a main osztályban

--- calculator_app/src/calculator_app/RFT_Calculator.java
+++ calculator_app/src/calculator_app/RFT_Calculator.java
@@ -6,6 +6,10 @@ import javax.swing.*;
 public class RFT_Calculator {
 
     public static void main(String[] args)throws Exception{
+        WelcomeWindow ww = new WelcomeWindow(){};
+        ww.setTitle("Üdvözlet");
+        ww.setAlwaysOnTop(true);
+        
         CalculatorView calc = new CalculatorView(){};
         calc.setTitle("Szamologep");
         calc.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
